10eaa7f5e7dc7507d93b38367a93ca998e13aad4
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2021-01-24 10:41:51 -0500

    fix type

--- gitTest/src/git/test/Student.java
+++ gitTest/src/git/test/Student.java
@@ -2,9 +2,12 @@ package git.test;
 
 public class Student {
 	int id;
-	
 	String name;
-	
+	 
+	public Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 	public int getId() {
 		return id;
 	}
